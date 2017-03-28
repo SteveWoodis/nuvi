7227542e16b153dd21eb222adce34777118a82be
Steve Woodis <swoodis2010@gmail.com>
2017-03-28 00:06:35 -0600

    Things are looking better. Fetch is pulling data. Almost ready for UI

--- src/users.js
+++ src/users.js
@@ -1,31 +1,48 @@
 import React, {Component} from 'react';
-let newRecords = [];
 class Users extends Component{
   constructor(){
     super();
     this.state= {items: []}
+    this.getArray = this.getArray.bind(this);
   }
   getArray(){
-    return fetch('https://nuvi-challenge.herokuapp.com/activities')
+    let self = this;
+    fetch('https://nuvi-challenge.herokuapp.com/activities')
       .then(function (response) {
         return response.json()
-      }).then(function (json) {
-        newRecords = json;
-        //newRecords.forEach((item) => this.setState({items: item}));
-        console.log("First Array ",newRecords);
-       return newRecords;
-      })
-}
-  componentWillMount(){
-    this.getArray();
+      }).then(function (items) {
+      self.setState({items})
+    }).catch(function (err) {
+      //todo
+      console.log("This is an error", err)
+    })
   }
+
+  componentWillMount(){
+    let newRecords = [];
+    newRecords = this.getArray();
+    console.log(newRecords);
+      }
+
 
 
   render(){
     let items = this.state.items;
+    //let i = 0;
     return(
       <div>
-        {items.map(item => (<h5 key={item.id} style={{marginLeft: '15px'}}>{item.actor_username}</h5>))}
+        {items.map(item => (
+        <ul style={{textDecoration: 'none'}}>
+
+          <li style={{textDecoration: 'none'}}><h6 key={item.id} style={{marginLeft: '15px'}}>{item.provider}</h6></li>
+          <li style={{textDecoration: 'none'}}><h6>URL: <a href={item.activity_url}>{item.activity_url}</a></h6></li>
+          <li style={{textDecoration: 'none'}}><h6>Actor's Username: {item.actor_username}</h6></li>
+          <li style={{textDecoration: 'none'}}><h6>ID: {item.id}</h6></li>
+          <li style={{textDecoration: 'none'}}><h5>Name: {item.actor_name}</h5></li>
+          
+        </ul>
+        ))}
+
       </div>
     )
   }
